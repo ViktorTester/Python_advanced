aba1525d7bfbc6fe999a6ac725b82a81c449b749
VIktor <viktor345@inbox.lv>
2022-11-02 20:10:50 +0200

    8_4_14 added

---
+++ Python_advanced/sets/8_4_14.py
@@ -0,0 +1,8 @@
+one_string = input() + input()
+if len(set(one_string)) < 10:
+    print('NO')
+else:
+    print('YES')
+
+# The input to the program is two strings consisting of numbers. It is necessary
+# to determine whether it is true that all ten digits are used in the record of these two lines?
