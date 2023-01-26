8849cc4997297754fc2ce1521d2496b14938a433
VIktor <viktor345@inbox.lv>
2023-01-26 19:45:13 +0200

    files 15_4_11, 15_4_12 and 15_4_13 added

---
+++ Python_advanced/functions/15_4_12.py
@@ -0,0 +1,11 @@
+numbers = [(10, 10, 10), (30, 45, 56), (81, 80, 39), (1, 2, 3), (12, 45, 67), (-2, -4, 100), (1, 2, 99), (89, 90, 34),
+           (10, 20, 30), (50, 40, 50), (34, 78, 65), (-5, 90, -1)]
+
+def avg_numbers(number):
+    return min(number) + max(number)
+
+print(sorted(numbers, key=avg_numbers))
+
+# Given a list of numbers containing tuples of numbers. The program sorts and
+# displays the list of numbers according to the sum
+# of the minimum and maximum elements of the tuple.
