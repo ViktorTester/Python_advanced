e810cd4943d26fd60a9113304434d2e9e60423ff
VIktor <viktor345@inbox.lv>
2023-02-23 11:04:53 +0200

    17_3_13 and 17_3_14 files added

---
+++ Python_advanced/file_handling/17_3_14.py
@@ -0,0 +1,12 @@
+with open('population.txt', 'r') as file:
+    text = [line.strip().split('\t') for line in file.readlines()]
+
+    for country in text:
+        if country[0].startswith('G') and int(country[-1]) > 500_000:
+            print(country[0])
+
+# There is a population.txt file with the names of countries and
+# their population, separated by the tab character '\t'.
+
+# The program displays all countries whose name begins with the letter 'G'
+# and whose population is greater than 500,000 people, without changing their order.
